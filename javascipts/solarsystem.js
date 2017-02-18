894d36c92e74c8d256f31db3121a0c77ed437336
Sanyyouisf <sanymamdoh@yahoo.com>
2017-02-18 11:14:10 -0600

    adding all augmentors

--- javascipts/solarsystem.js
+++ javascipts/solarsystem.js
@@ -1,19 +1,8 @@
 var SolarSystem = (function (){
 	
-	var planets = ["mercury","venus","earth","marth","jupiter","uranus","neptune"];
 	var dwarfPlanets = ["pluto"];
-	var numPlanetsPeopleLandOn=0;
-
+	
 	return {
-		getPlanets: function(){		
-			return planets;
-		},
-		getPlanetsLandedOn : function(){
-			return numPlanetsPeopleLandOn;
-		},
-		setPlanetsLandedOn : function(){
-			numPlanetsPeopleLandOn ++;
-		},
 		getDwarfPlanets : function(){
 			return "they are rejects";
 		},
@@ -24,15 +13,6 @@ var SolarSystem = (function (){
 
 })();
 
-// console.log (" planets: ", SolarSystem.gerPlanets());
-// console.log (" planets with people: ", SolarSystem.getPlanetsLandedOn());
-// console.log(" spaceShips: ",SolarSystem.getspaceShips());
-
-// SolarSystem.setspaceShips("voyger1")
-// SolarSystem.setspaceShips("voyger2")
-// SolarSystem.setspaceShips("Gemini")
-// SolarSystem.setspaceShips("Appollo")
-// console.log(" spaceShips: ",SolarSystem.getspaceShips());
 
 
 
